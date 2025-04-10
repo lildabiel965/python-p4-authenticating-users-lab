c999b7b2af75ee9ee67cccb57497be7f2c6685e5
Daniel <daniel.mutembei@student.moringaschool.com>
2025-04-10 08:21:35 +0300

    done

--- server/app.py
+++ server/app.py
@@ -18,14 +18,34 @@ db.init_app(app)
 
 api = Api(app)
 
-class ClearSession(Resource):
+class Login(Resource):
+    def post(self):
+        username = request.json.get('username')
+        user = User.query.filter_by(username=username).first()
+        if user:
+            session['user_id'] = user.id
+            return make_response(jsonify(user.to_dict()), 200)
+        return {'error': 'User not found'}, 404
 
+class Logout(Resource):
     def delete(self):
-    
+        session['user_id'] = None
+        return {}, 204
+
+class CheckSession(Resource):
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id:
+            user = User.query.get(user_id)
+            return make_response(jsonify(user.to_dict()), 200)
+        return {}, 401
+
+class ClearSession(Resource):
+    def delete(self):
         session['page_views'] = None
         session['user_id'] = None
-
         return {}, 204
+
 
 class IndexArticle(Resource):
     
@@ -48,6 +68,9 @@ class ShowArticle(Resource):
 
         return {'message': 'Maximum pageview limit reached'}, 401
 
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
